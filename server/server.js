1829aedf10ccf17848a5f7147acee30cb564d1ec
fireflyq <1035394794@qq.com>
2020-03-23 01:06:20 +0800

    add login and register modal

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const usersRouter = require('./routes/users')
 
-const mongoose = require('mongoose')
-const DB_URL = 'mongodb://localhost:27017/blog'
-mongoose.connect(DB_URL)
-mongoose.connection.on('connected', function() {
-  console.log('mongo connect success')
-})
+// const mongoose = require('mongoose')
+// const DB_URL = 'mongodb://localhost:27017/blog'
+// mongoose.connect(DB_URL)
+// mongoose.connection.on('connected', function() {
+//   console.log('mongo connect success')
+// })
 const app = express()
 
 app.use('/users', usersRouter);
